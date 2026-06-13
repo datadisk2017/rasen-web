61f09a5a6ff3e0f32b234fd5f9be3658d7a9220e
datadisk2017 <datadisk2017@users.noreply.github.com>
2026-06-13 14:14:04 +0900

    Fix image extension in page1

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/rasen-web',
+  basePath: isProd ? '/rasen-web' : '',
   images: {
     unoptimized: true,
   },
